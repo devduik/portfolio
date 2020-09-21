30db568b7761b66ecac796fb07e2af5509c670e3
dewaduik <dewa.dwian@gmail.com>
2020-09-21 22:14:36 +0800

    update scroll animate

--- js/style.js
+++ js/style.js
@@ -1,65 +1,63 @@
 $(document).ready(function () {
-  $('body').scrollspy({ target: ".navbar", offset: 50 });
+    $('body').scrollspy({ target: ".navbar", offset: 50 });
 
-  $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
 
-  $("#myNavbar a").on('click', function (event) {
-    if (this.hash !== "") {
-      var hash = this.hash;
-      
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
-        window.location.hash = hash;
-      });
-    }
-    
-    event.preventDefault();
-  });
+    $("#myNavbar a").on('click', function (event) {
+        if (this.hash !== "") {
+            var hash = this.hash;
 
-  $.getJSON('data/project.json', function (data) {
-
-    $('.project_list').html("");
-
-    $.each(data, function (i, project) {
-
-      let roles = "";
-      $.each(project.role, function (i, role) {
-        roles += '<li>' + role + '</li>';
-      });
-
-      let tools = "";
-      $.each(project.tool, function (i, tool) {
-        tools += '<a class="tool">' + tool + '</a> ';
-      });
-
-      $('.project_list').append(`
-
-        <div class="col-sm-6">
-          <div class="item">
-      
-            <div class="item-main" data-toggle="collapse" data-target="#project`+ project.id + `">
-              <h3 class="item-title">`+ project.name + ` <small>(` + project.type + `)</small></h3>
-              <i>~ `+ project.title + `</i>
-        
-              <div id="project`+ project.id + `" class="collapse">
-                <p class="item-detail">`+ project.description + `</p>
-                <label>My role:</label>
-                <ul>`+ roles + `</ul>
-              </div>
-            </div>
-        
-            <div class="item-info">
-              <span>Build with:</span>
-              `+ tools + `
-            </div>
-        
-          </div>
-        </div>
-
-      `);
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+        event.preventDefault();
     });
 
-  });
+    $.getJSON('data/project.json', function (data) {
+
+        $('.project_list').html("");
+
+        $.each(data, function (i, project) {
+
+            let roles = "";
+            $.each(project.role, function (i, role) {
+                roles += '<li>' + role + '</li>';
+            });
+
+            let tools = "";
+            $.each(project.tool, function (i, tool) {
+                tools += '<a class="tool">' + tool + '</a> ';
+            });
+
+            $('.project_list').append(`
+    
+                <div class="col-sm-6">
+                    <div class="item">
+                    
+                        <div class="item-main" data-toggle="collapse" data-target="#project`+ project.id + `">
+                            <h3 class="item-title">`+ project.name + ` <small>(` + project.type + `)</small></h3>
+  
+                            <i>~ `+ project.title + `</i>
+  
+                            <div id="project`+ project.id + `" class="collapse">
+                                <p class="item-detail">`+ project.description + `</p>
+                                <label>My role:</label>
+                                <ul>`+ roles + `</ul>
+                            </div>
+                        </div>
+                    
+                        <div class="item-info">
+                            <span>Build with:</span>
+                            `+ tools + `
+                        </div>
+                    
+                    </div>
+                </div>
+  
+            `);
+        });
+
+    });
 
 });
