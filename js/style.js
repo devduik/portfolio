aeab121f8c7b20631c59902b4866c3e3d562f139
devduix <50471037+devduix@users.noreply.github.com>
2020-09-21 21:29:45 +0800

    Update style.js

--- js/style.js
+++ js/style.js
@@ -5,16 +5,16 @@ $(document).ready(function () {
 
   $("#myNavbar a").on('click', function (event) {
     if (this.hash !== "") {
-      event.preventDefault();
-
       var hash = this.hash;
-
+      
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function () {
         window.location.hash = hash;
       });
     }
+    
+    event.preventDefault();
   });
 
   $.getJSON('data/project.json', function (data) {
